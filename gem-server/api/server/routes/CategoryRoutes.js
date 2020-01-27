5efa1df1acf731ef0dcf98d674e66c0da947cba5
Vaselek <burzhuas@gmail.com>
2020-01-27 22:47:52 +0600

    Gem-server: Update creation of Category

--- gem-server/api/server/routes/CategoryRoutes.js
+++ gem-server/api/server/routes/CategoryRoutes.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import CategoryController from '../controllers/CategoryController';
 import auth from '../middleware/auth';
 import permit from "../middleware/permit";
+import upload from "../../multer";
+
 
 const router = Router();
 
 router.get('/', CategoryController.getAllCategories);
-router.post('/', [auth, permit('admin')], CategoryController.addCategory);
+router.post('/', [auth, permit('admin'), upload.none()], CategoryController.addCategory);
 router.get('/:id', CategoryController.getACategory);
 router.put('/:id', [auth, permit('admin')], CategoryController.updatedCategory);
 router.delete('/:id', [auth, permit('admin')], CategoryController.deleteCategory);
