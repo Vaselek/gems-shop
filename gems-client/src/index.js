107364c6f988459d7045d5bf0acd589f454a8b76
Vaselek <burzhuas@gmail.com>
2020-01-29 10:43:14 +0600

    Gems-client: Add react-bootstrap-table2 and octicon library for icons

--- gems-client/src/index.js
+++ gems-client/src/index.js
@@ -4,6 +4,8 @@ import {Provider} from 'react-redux';
 import {ConnectedRouter} from 'connected-react-router';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+
 
 import App from './App';
 import store, {history} from './store/configureStore';
