707558ce2507c63fb101a88e48177e3008ed3994
Vaselek <burzhuas@gmail.com>
2020-02-03 21:57:33 +0600

    Gems-client: Add filter to GemsTable

--- gems-client/src/containers/GemsTable/GemsTable.js
+++ gems-client/src/containers/GemsTable/GemsTable.js
@@ -1,30 +1,47 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 import {useSelector, useDispatch} from 'react-redux';
 import {fetchGems} from "../../store/actions/gemsActions";
-import {apiURL} from "../../constants";
+import {apiURL, defaultGemParams} from "../../constants";
 import './GemsTable.css';
 import Octicon, {Pencil, Trashcan} from '@primer/octicons-react'
 import {useHistory} from "react-router";
 import paginationFactory from 'react-bootstrap-table2-paginator';
+import 'react-bootstrap-table2-filter/dist/react-bootstrap-table2-filter.min.css';
+import filterFactory, { selectFilter, multiSelectFilter } from 'react-bootstrap-table2-filter';
+import {fetchStones} from "../../store/actions/stonesActions";
+import {fetchMetals} from "../../store/actions/metalsActions";
+import {fetchCoatings} from "../../store/actions/coatingsActions";
+import {fetchCategories} from "../../store/actions/categoriesActions";
+import { isEmpty } from 'lodash';
 
 
 const GemsTable = () => {
   const gems = useSelector(state => state.gems.gems);
   const totalCount = useSelector(state => state.gems.totalCount);
-  const page = useSelector(state => state.gems.gemParams.pagination.offset) || 1;
-  const sizePerPage = useSelector(state => state.gems.gemParams.pagination.limit) || 10;
+  const gemParams = useSelector(state => state.gems.gemParams);
   const dispatch = useDispatch();
   const history = useHistory();
+  const [ page,setPage ] = useState(1);
+  const stones = useSelector(state => state.stones.stones);
+  const metals = useSelector(state => state.metals.metals);
+  const coatings = useSelector(state => state.coatings.coatings);
+  const categories = useSelector(state => state.categories.categories);
+
 
   useEffect(() => {
-    let pagination = {}
-    pagination.limit = 10
-    dispatch(fetchGems({pagination}))
-  }, [dispatch]);
+    const newGemParams = { ...defaultGemParams };
+    newGemParams.pagination.limit = 10;
+    dispatch(fetchGems(newGemParams));
+    if (isEmpty(stones)) dispatch(fetchStones());
+    if (isEmpty(metals)) dispatch(fetchMetals());
+    if (isEmpty(coatings)) dispatch(fetchCoatings());
+    if (isEmpty(categories)) dispatch(fetchCategories());
+  }, [dispatch, stones, metals, coatings, categories]);
 
   function categoryFormatter(cell, row, rowIndex, formatExtraData) {
-    const content = cell.length === 0 ? '' : cell.map(item => item.title).join(', ')
+    const content = cell.length === 0 ? '' : cell.map(item => item.title).join(', ');
+
     return (
       <span>{content}</span>
     );
@@ -52,52 +69,103 @@ const GemsTable = () => {
     )
   }
 
+  const itemSelectOptions = (items) => {
+    const selectOptions = {};
+    items.map(item => {
+      const key = item['id'];
+      const value = item['title']
+      selectOptions[key] = value;
+    });
+    return selectOptions;
+  }
+
   const columns = [{
     dataField: 'id',
     text: 'ID',
     classes: 'gems-table-id-column',
+    sort: true,
     headerStyle: (column, colIndex) => {
-      return { width: '5%' };
+      return { width: '6%' };
     }
   }, {
     dataField: 'title',
-    text: 'Название'
+    text: 'Имя',
+    sort: true,
+    headerStyle: (column, colIndex) => {
+      return { width: '7%' };
+    },
   }, {
     dataField: 'description',
-    text: 'Описание'
+    text: 'Детали',
+    headerStyle: (column, colIndex) => {
+      return { width: '8%' };
+    },
   }, {
     dataField: 'price',
     text: 'Цена',
+    sort: true,
     headerStyle: (column, colIndex) => {
-      return { width: '5%' };
+      return { width: '8%' };
     }
   }, {
     dataField: 'weight',
     text: 'Вес',
+    sort: true,
     headerStyle: (column, colIndex) => {
-      return { width: '5%' };
+      return { width: '7%' };
     }
   }, {
     dataField: 'image',
     text: 'Рис.',
     classes: 'gems-table-image-column',
-    formatter: imageFormatter
+    formatter: imageFormatter,
+    headerStyle: (column, colIndex) => {
+      return { width: '6%' };
+    },
   }, {
     dataField: 'categories',
     formatter: categoryFormatter,
-    text: 'Категория'
+    text: 'Вид',
+    filter: selectFilter({
+      options: itemSelectOptions(categories),
+      placeholder: 'Выбрать категорию'
+    }),
+    headerStyle: (column, colIndex) => {
+      return { width: '7%' };
+    },
   }, {
     dataField: 'stones',
     text: 'Камни',
-    formatter: categoryFormatter
+    formatter: categoryFormatter,
+    filter: multiSelectFilter({
+      options: itemSelectOptions(stones),
+      placeholder: 'Фильтр'
+    }),
+    headerStyle: (column, colIndex) => {
+      return { width: '6%' };
+    },
   }, {
     dataField: 'metals',
     text: 'Металл',
-    formatter: categoryFormatter
+    formatter: categoryFormatter,
+    filter: multiSelectFilter({
+      options: itemSelectOptions(metals),
+      placeholder: 'Фильтр'
+    }),
+    headerStyle: (column, colIndex) => {
+      return { width: '8%' };
+    },
   }, {
     dataField: 'coatings',
     text: 'Покрытие',
-    formatter: categoryFormatter
+    formatter: categoryFormatter,
+    filter: multiSelectFilter({
+      options: itemSelectOptions(coatings),
+      placeholder: 'Фильтр'
+    }),
+    headerStyle: (column, colIndex) => {
+      return { width: '10%' };
+    },
   }, {
     dataField: 'id1',
     text: 'Изменить',
@@ -128,21 +196,44 @@ const GemsTable = () => {
       }
     }
   }];
-
-  const handleTableChange = (type, { page, sizePerPage }) => {
-    if (type === 'pagination') {
-      const pagination = {};
-      pagination.limit = sizePerPage;
-      pagination.offset = (page - 1) * sizePerPage;
-      dispatch(fetchGems({pagination}))
+  const handleTableChange = (type, { page, sizePerPage, sortField, sortOrder, filters}) => {
+    if (gemParams) {
+      const newGemParams = { ...gemParams };
+      if (type === 'pagination') {
+        setPage(page);
+        newGemParams.pagination.limit = sizePerPage;
+        newGemParams.pagination.offset = (page - 1) * sizePerPage;
+      }
+      if (type === 'sort') {
+        newGemParams.sort = {field: sortField, order: sortOrder}
+      }
+      if (type === 'filter') {
+        if (filters.stones) {
+          newGemParams.filter['stoneIds'] = [];
+          filters.stones.filterVal.map(val => newGemParams.filter.stoneIds.push(val))
+        }
+        if (filters.metals) {
+          newGemParams.filter['metalIds'] = [];
+          filters.metals.filterVal.map(val => newGemParams.filter.metalIds.push(val))
+        }
+        if (filters.coatings) {
+          newGemParams.filter['coatingIds'] = [];
+          filters.coatings.filterVal.map(val => newGemParams.filter.coatingIds.push(val))
+        }
+        if (filters.categories) {
+          newGemParams['categoryId'] = [filters.categories.filterVal];
+        }
+      }
+      dispatch(fetchGems(newGemParams))
     }
   };
 
-  const options = {
+  const paginationOptions = {
     page: page,
-    sizePerPage: sizePerPage,
+    sizePerPage: gemParams.pagination ? gemParams.pagination.limit || 10 : 10,
     totalSize: totalCount
-  }
+  };
+
 
   return (
     <div>
@@ -154,8 +245,9 @@ const GemsTable = () => {
                       condensed
                       keyField='id'
                       data={ gems }
-                      pagination={ paginationFactory(options) }
-                      remote
+                      pagination={ paginationFactory(paginationOptions) }
+                      filter={ filterFactory() }
+                      remote={ { pagination: true, sort: true, filter: true } }
                       onTableChange={ handleTableChange }
                       columns={ columns } />
     </div>
