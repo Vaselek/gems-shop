12d11c9969f56c4590413d4dc0680ead90463fde
Vaselek <burzhuas@gmail.com>
2020-02-05 17:20:06 +0600

    Gems-client: Reset error if stone is added successfully

--- gems-client/src/store/reducers/coatingsReducer.js
+++ gems-client/src/store/reducers/coatingsReducer.js
@@ -1,4 +1,8 @@
-import { FETCH_COATINGS_SUCCESS, CREATE_COATING_FAILURE } from "../actions/coatingsActions";
+import {
+  FETCH_COATINGS_SUCCESS,
+  CREATE_COATING_FAILURE,
+  CREATE_COATING_SUCCESS
+} from "../actions/coatingsActions";
 
 const initialState = {
   coatings: [],
@@ -12,6 +16,8 @@ const coatingsReducer = (state = initialState, action) => {
       return {...state, coatings: action.coatings};
     case CREATE_COATING_FAILURE:
       return {...state, error: action.error};
+    case CREATE_COATING_SUCCESS:
+      return {...state, error: null};
     default:
       return state;
   }
